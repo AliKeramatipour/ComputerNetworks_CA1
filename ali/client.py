7eca2f7619c6df757108369a1d709d5ed4d39e9f
Ali Keramatipour <alikeramatipour@gmail.com>
2020-04-07 21:48:42 +0430

    added thread control functions

--- ali/client.py
+++ ali/client.py
@@ -1,17 +1,48 @@
+import time
 from socket import *
+import threading
 
-
-MAXLISTEN = 15
 SERVERPORT = 9000
+MAXLISTEN = 15
+EOF = chr(26)
 MAXMSGLEN = 1000
-
-
-
 #--------------------------------------------------------------
+
+def recvNextMsg(msgSocket, inputBuffer):
+    data = left = right = ""
+    try:
+        while True:
+            inputBuffer = inputBuffer + data
+            data = msgSocket.recv(MAXMSGLEN)
+            for c in data:
+                if c == EOF:
+                    left,right = data.split(EOF,1)
+                    msg = inputBuffer + left
+                    inputBuffer = right
+                    return msg, inputBuffer
+    except KeyboardInterrupt:
+        if msgSocket:
+            msgSocket.close()
+        sys.exit()
+    
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+
 PORT = int(input("Enter port number:"))
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.bind(("", PORT))
-clientSocket.connect(("",SERVERPORT))
-data = clientSocket.recv(MAXMSGLEN)
+msgSocket = socket(AF_INET, SOCK_STREAM)
+msgSocket.bind(("", PORT))
+msgSocket.connect(("",SERVERPORT))
+inputBuffer = ""
+
+print("here?")
+data, inputBuffer = recvNextMsg(msgSocket, inputBuffer)
+print(data)
 for i in data:
-    print(int(i))
+    print(ord(i))
+print("stuck here?")
+data, inputBuffer = recvNextMsg(msgSocket, inputBuffer)
+print("nah not stuck")
+print(data)
+for i in data:
+    print(ord(i))
+msgSocket.close()
--- ali/server.py
+++ ali/server.py
@@ -1,15 +1,62 @@
+import time
 from socket import *
-
+import sys
+import threading
 
 PORT = 9000
 MAXLISTEN = 15
+EOF = chr(26)
+MAXMSGLEN = 1000
 #--------------------------------------------------------------
+
+def recvNextMsg(msgSocket, inputBuffer):
+    data = ""
+    while True:
+        inputBuffer = inputBuffer + data
+        data = msgSocket.recv(MAXMSGLEN)
+        for c in data:
+            if ord(c) == EOF:
+                left, right = data.split(EOF,1)
+                msg = inputBuffer + left
+                inputBuffer = right
+                return msg, inputBuffer
+
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+
+def handle_client(msgSocket):
+    print("inside new thread!")
+    inputBuffer = ""
+    msgSocket.sendall("Welcome to your FTP server! \nEnter USERNAME:" + EOF)
+    newMsg, inputBuffer = recvNextMsg(msgSocket, inputBuffer)
+    msgSocket.sendall("NEW" + newMsg + "NEW" + EOF)
+    msgSocket.close()
+
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+
+def on_press(key):
+    print (key)
+
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+
 serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 serverSocket.bind(("", PORT))
 serverSocket.listen(MAXLISTEN)
 while True:
-    print ("server listening")
-    newSocket, address = serverSocket.accept()
-    print ("recieved connection from",address)
-    newSocket.send("hello")
-    newSocket.close()
+    try:
+        print ("server listening")
+        msgSocket, address = serverSocket.accept()
+        print ("creating new connection")
+        t = threading.Thread(target = handle_client, args = (msgSocket,))
+        t.setDaemon(True)
+        t.start()
+        print ("recieved connection from",address)
+    except KeyboardInterrupt:
+        if serverSocket:
+            print("got Keyboard interrupt in main Program!")
+            serverSocket.close()
+            sys.exit()
+
